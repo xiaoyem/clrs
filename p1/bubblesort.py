0412fd3aa62660ef7152d97c6299296dcdb8befc
Doriszhaotutu <761725461@qq.com>
2016-04-08 20:05:23 +0800

    add copyright

--- p1/bubblesort.py
+++ p1/bubblesort.py
@@ -2,6 +2,22 @@
 # -*- coding:utf-8 -*-
 #Author : ZhaoYuchao
 #2016-04-08 16:23:16
+#
+#Copyright (c) 2005-2016 by Xiaoye Meng.
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 def bubblesort(A):
     for i in range(0,len(A)):
         for j in range(len(A)-1,i,-1):
--- p1/merge_sort.py
+++ p1/merge_sort.py
@@ -2,7 +2,22 @@
 # -*- coding:utf-8 -*-
 #Author : ZhaoYuchao
 #2016-04-08 17:13:19
-
+#
+#Copyright (c) 2005-2016 by Xiaoye Meng.
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 def MERGE(A,p,q,r):
     L=A[p:q+1]
     R=A[q+1:r+1]
