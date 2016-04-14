97acc24675d797b10843253bbd86a4651d396971
Doriszhaotutu <761725461@qq.com>
2016-04-14 10:46:20 +0800

    test permute_by_sorting

---
+++ test/test_permute_by_sorting.py
@@ -0,0 +1,26 @@
+
+#
+# Copyright (c) 2016 by Zhao Yuchao.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -*- coding:utf-8 -*-
+
+
+m os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from p1.permute_by_sorting import permute_by_sorting
+
+a = [1,2,3,4,5,6,7,8,9]
+print(permute_by_sorting(a))
