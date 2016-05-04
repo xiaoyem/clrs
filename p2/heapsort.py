d26e4e9d6d199e53472a5fd515ca34d7a99e55a0
Doriszhaotutu <761725461@qq.com>
2016-05-04 11:30:39 +0800

    heapsort with py

---
+++ p2/heapsort.py
@@ -0,0 +1,47 @@
+
+#
+# Copyright (c) 2016 by Zhao Yuchao.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -*- coding:utf-8 -*-
+
+def max_heapify(a, i, aheapsize) :
+    l = 2 * i + 1
+    r =2 * i + 2
+    if l <= aheapsize and a[l] > a[i] :
+        largest = l
+    else :
+        largest = i
+    if r <= aheapsize and a[r] > a[largest] :
+        largest = r
+    if largest != i :
+        a[i], a[largest] = a[largest], a[i]
+        max_heapify(a, largest, aheapsize)
+
+def build_max_heap(a) :
+    aheapsize = len(a)
+    for i in range(len(a) / 2, - 1, - 1) :
+        max_heapify(a, i, aheapsize - 1)
+
+def heapsort(a) :
+    build_max_heap(a)
+    aheapsize = len(a)
+    for i in range(len(a) - 1, 0, - 1) :
+        a[0], a[i] = a[i], a[0]
+        aheapsize = aheapsize - 1
+        max_heapify(a, 0, aheapsize - 1 )
+        
+
+
