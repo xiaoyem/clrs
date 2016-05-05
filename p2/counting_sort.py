22e617c4eeef4f5cfb183792075e404495ebe704
Doriszhaotutu <761725461@qq.com>
2016-05-05 15:32:48 +0800

    counting sort with python

---
+++ p2/counting_sort.py
@@ -0,0 +1,32 @@
+
+#
+# Copyright (c) 2016 by Zhao Yuchao.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -*- coding:utf-8 -*-
+
+def counting_sort(a) :
+    k = max(a)
+    c = [0] * (k + 1)
+    b = [0] * len(a)
+    for j in range(0, len(a)) :
+        c[a[j]] = c[a[j]] + 1
+    for i in range(1, k + 1) :
+        c[i] = c[i] + c[i - 1]
+    for j in range(len(a) - 1, 0, -1 ) :
+        b[c[a[j]] - 1] = a[j]
+        c[a[j]] = c[a[j]] - 1
+    return b
+
