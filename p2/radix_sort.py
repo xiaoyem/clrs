4e88abf014165df90fbb0741f819ef2b7f933c07
Doriszhaotutu <761725461@qq.com>
2016-05-06 14:45:36 +0800

    radix sort with python

---
+++ p2/radix_sort.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2016 by Yuchao Zhao.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import operator
+
+def radix_sort(a, d):
+    for i in range(1, d + 1):
+        a = sort_by_i(a, i)
+    return a
+def sort_by_i(a, i):
+    p = [0] * len(a)
+    for j in range(len(a)):
+        p[j] = (a[j]/pow(10, (i - 1))) % 10
+    link = sorted(zip(p, a), key = operator.itemgetter(0))
+    a = map(operator.itemgetter(1), link)
+    return a
